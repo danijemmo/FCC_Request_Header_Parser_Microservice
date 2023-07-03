50f3e6b5bc96ad3c2cd8b25d197ed5904cc4e1a7
Daniel jemmo <Danieljemmo1@gmail.com>
2023-07-03 05:36:27 -0700

    Request-Header-Parser-Microservice

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({ 
+    ipaddress: req.socket.remoteAddress,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+    
+  });
 });
 
 // listen for requests :)
